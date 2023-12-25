7010c7a5690e1db51969a11822f0cc97d7fd04e0
Tahaak67 <taha699@gmail.com>
2023-12-25 19:28:34 +0200

    Update metadata Bump version codes

--- buildSrc/src/main/java/ProjectConfig.kt
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -3,6 +3,6 @@ object ProjectConfig {
     const val compileSdk = 34
     const val minSdk = 23
     const val targetSdk = 33
-    const val versionCode = 5
-    const val versionName = "0.1.3"
+    const val versionCode = 6
+    const val versionName = "0.2.0"
 }
